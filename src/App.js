3a0ca4374d372b1e54058df59b6146fe9fab2d40
ayman saleh <ayman.saleh000@gmail.com>
2024-04-25 14:44:18 +0200

    modyfying

--- src/App.js
+++ src/App.js
@@ -55,18 +55,27 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+      <Logo />
+      <Search />
+      <NumResults movies={movies} />
+       </NavBar>
+
+      <Main>
+      <ListBox>
+      <MovieList movies={movies} />
+        </ListBox>
+        
+      <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function NavBar( {movies }) {
+function NavBar( { children }) {
   return (
     <nav className="nav-bar">
-      <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      { children }
     </nav>
   );
 }
@@ -103,16 +112,15 @@ function NumResults( {movies }) {
   );
 }
 
-function Main({ movies }) {
+function Main({ children }) {
   return (
     <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
+      { children }
     </main>
   );
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -123,7 +131,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children }
     </div>
   );
 }
@@ -175,7 +183,7 @@ function WatchedBox() {
   );
 }
 
-function WachtedSummary({watched}) {
+function WachtedSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
@@ -205,8 +213,7 @@ function WachtedSummary({watched}) {
   );
 }
 
-function WachtedList ({watched}) {
-
+function WachtedList ({ watched }) {
 return (
           <ul className="list">
             {watched.map((movie) => (
