dcaddc47a67871ea0db374387ced6a3369acc418
ayman saleh <ayman.saleh000@gmail.com>
2024-04-27 18:46:27 +0300

    modyfying

--- src/StarsRating.js
+++ src/StarsRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Proptypes from 'prop-types'
 
 const continerStyle = {
 display:'flex',
@@ -10,26 +11,42 @@ const starContinerStyle = {
  display: 'flex',
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0'
-};
+StarsRating.prototype = {
+  maxrating: Proptypes.number,
+  size: Proptypes.number,
+  color: Proptypes.string,
+  className: Proptypes.string,
+  messages: Proptypes.array,
+  defaultrating: Proptypes.number,
+  onSetRating: Proptypes.func
+}
 
-const starStyle ={
- width: '48px',
- height: '48px',
- display: 'block',
- cursor: 'pointer'
-};
-
-export default function StarsRating ({maxrating = 5}) {
-    const [rating, setRating] = useState(0);
+export default function StarsRating ({ 
+  maxrating = 5,
+  size = 48,
+  color = '#fcc449',
+  className = '',
+  messages = [],
+  defaultrating = 0,
+  onSetRating,
+}) {
+    const [rating, setRating] = useState(defaultrating);
     const [tempRating, setTempRating] = useState(0);
+
     function handelRating(rating) {
       setRating(rating);
+      onSetRating(rating);
     }
+
+    const textStyle = {
+      lineHeight: '1',
+      margin: '0',
+      color,
+      fontSize: `${size/1.5}px` ,
+    };
+    
     return (
-       <div style={continerStyle}>
+       <div style={continerStyle} className='test'>
         <div style={starContinerStyle}>
        {Array.from({length: maxrating}, (_, i) => 
        <Stars 
@@ -38,16 +55,28 @@ export default function StarsRating ({maxrating = 5}) {
        onHaverin={() => setTempRating(i + 1) }
        onHaverOut={() => setTempRating(0)}
        full={tempRating ? tempRating >= i + 1 :   rating >= i + 1 }
+       color={color}
+       size={size}
        />
     )}
        </div>
-      <p style={textStyle}>{tempRating || rating || "" }</p>
+      <p style={textStyle}>{messages.length === maxrating ? messages[tempRating ? tempRating - 1 : rating - 1]  :  tempRating || rating || "" }</p>
         </div>
     );
 }
 
 
-function Stars ({onRate, full, onHaverin, onHaverOut}) {
+function Stars ({
+  onRate, full, onHaverin, onHaverOut, color, size}) {
+
+    const starStyle ={
+      width: `${size}px`,
+      height: `${size}px`,
+      display: 'block',
+      cursor: 'pointer',
+     };
+     
+
     return (
     <span   
     role="button"
@@ -61,8 +90,8 @@ function Stars ({onRate, full, onHaverin, onHaverOut}) {
 <svg
   xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 20 20"
-  fill="#000"
-  stroke="#000"
+  fill={color}
+  stroke={color}
 >
   <path
     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -74,7 +103,7 @@ function Stars ({onRate, full, onHaverin, onHaverOut}) {
   xmlns="http://www.w3.org/2000/svg"
   fill="none"
   viewBox="0 0 24 24"
-  stroke="#000"
+  stroke={color}
 >
   <path
     strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import StarsRating from './StarsRating';
 // import './index.css';./StarsRating
 // import App from './App';
 
+function Test () {
+  const [moveiRating, setMoveiRating] = useState(0);
+
+  return (
+  <div>
+      <StarsRating color='blue' maxrating={10} onSetRating={setMoveiRating} />
+    <p>This movei was rated {moveiRating} stars</p>
+  </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarsRating maxrating={5} />
+    <StarsRating 
+    maxrating={5} 
+    messages={['Treibble', 'bad', 'Good', 'Okay', 'Ecxellent']}
+    />
+    <StarsRating 
+    size = '24' 
+    color='red' 
+    className='test' 
+    defaultrating={2}
+    />
+    <Test  />
   </React.StrictMode>
 );
 
